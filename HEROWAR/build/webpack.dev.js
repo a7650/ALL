57ede39cf7f6b437220840509085d2fcbd6a8829
ZZP <43945674+a7650@users.noreply.github.com>
2021-01-20 13:06:51 +0800

    update

--- HEROWAR/build/webpack.dev.js
+++ HEROWAR/build/webpack.dev.js
@@ -5,28 +5,28 @@ const { constants } = require('buffer')
 const path = require('path')
 
 module.exports = merge(common, {
-    mode: 'development',
-    entry: {
-        app: ['webpack-hot-middleware/client', './src/index.ts']
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ],
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    stats: 'none'
+  mode: 'development',
+  entry: {
+    app: ['webpack-hot-middleware/client', './src/index.ts']
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  stats: 'none'
 })
--- HEROWAR/build/webpack.prod.js
+++ HEROWAR/build/webpack.prod.js
@@ -7,57 +7,57 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: 'source-map',
-    entry: {
-        app: './src/index.ts',
-    },
-    output: {
-        publicPath: './',
-        filename: '[name].[contenthash].js'
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new CleanWebpackPlugin({
-            dry: 'dist'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[id].css'
-        }),
-        new webpack.HashedModuleIdsPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader'
-                ]
-            }
+  mode: 'production',
+  devtool: 'source-map',
+  entry: {
+    app: './src/index.ts',
+  },
+  output: {
+    publicPath: './',
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new CleanWebpackPlugin({
+      dry: 'dist'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[id].css'
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
         ]
-    },
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new UglifyJSPlugin({
-                sourceMap: true,
-                exclude: /node_modules/
-            })
-        ],
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new UglifyJSPlugin({
+        sourceMap: true,
+        exclude: /node_modules/
+      })
+    ],
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
         }
-    },
+      }
+    }
+  },
 })
